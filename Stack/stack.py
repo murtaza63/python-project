bb0b2dad82de291814d4e1c109e6c90e59babb93
murtaza63 <murtaza63@gmail.com>
2022-08-30 02:10:58 +0500

    Stack is updated

--- Stack/stack.py
+++ Stack/stack.py
@@ -1,22 +1,26 @@
 
 
-
+# Creating Stack
 def stackData():
     stack = []
     return stack
+# Adding items into the stack
 def push(stack, item):
     return stack.append(item)
+#Removing an element from the stack
 def remove(stack):
     if checkEmpty(stack) == True:
         return "stack is empty"
     return stack.pop()
+# Creating an empty stack 
 def checkEmpty(stack):
     if len(stack) == 0:
         return True
     return False
+# Checking the size of the stack
 def checkSize(stack):
     return len(stack)
-
+# Getting the top element from the stack
 def checkPeek(stack):
     if checkEmpty(stack):
         return "Stack is empty so there is no peek element"
