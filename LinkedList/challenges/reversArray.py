bd84ec0b90afa65023c071046d134fd9ad73991f
murtaza63 <murtaza63@gmail.com>
2022-09-24 04:53:11 +0500

    LinkedList 1st challenge is added

--- LinkedList/challenges/reversArray.py
+++ LinkedList/challenges/reversArray.py
@@ -1,1 +1,30 @@
-arr =  
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def printInReverse(self, temp):
+        if temp:
+            self.printInReverse(temp.next)
+            print(temp.value, end=' ')
+        else:
+            return 
+    def push(self, item):
+        newNode = Node(item)
+        newNode.next = self.head
+        self.head = newNode
+
+
+list3 = LinkedList()
+
+list3.push(4)
+list3.push(3)
+list3.push(2)
+list3.push(1)
+
+list3.printInReverse(list3.head)
