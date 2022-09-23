6e1e51d229d43f8084a32e0e7c12fdb1f6bfc9d3
murtaza63 <murtaza63@gmail.com>
2022-09-24 02:01:30 +0500

    Queue with Stack implementation

---
+++ Queue/QueueStack.py
@@ -0,0 +1,35 @@
+from collections import deque
+class QueueStack:
+    
+
+    leftStack = []
+    rightStack = []
+
+    def isEmpty(self)->bool:
+        if len(self.leftStack) == 0 and len(self.rightStack) == 0:
+            return True 
+        else:
+            return False 
+    def peek(self):
+        if len(self.leftStack) != 0:
+            return self.leftStack[-1]
+        else:
+            return self.rightStack
+    def enqueue(self, element)-> bool:
+        self.rightStack.append(element)
+        return True 
+
+    def dequeue(self):
+        newStack = [value for value in self.leftStack if value == None]
+        if len(self.leftStack) == 0:
+            self.leftStack = self.rightStack[::-1]
+            newStack
+        return self.leftStack.pop(0)
+    
+
+queue = QueueStack()
+queue.enqueue("Ray")
+queue.enqueue("Brian")
+print(queue.enqueue(5))
+queue.dequeue()
+print(queue.peek())
