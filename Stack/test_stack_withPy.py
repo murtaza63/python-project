ae1587eaf938b8724bcc0c38d3346ae63d01a61b
murtaza63 <murtaza63@gmail.com>
2022-09-28 20:50:59 +0500

    Test added

--- Stack/test_stack_withPy.py
+++ Stack/test_stack_withPy.py
@@ -8,12 +8,9 @@ class TestClass:
     newStack = s.stackData()
     def test_empty(self):
         assert self.s.checkEmpty(self.newStack) == True
-
+   
     def test_checkSize(self):
         self.s.push(self.newStack,4)
         self.s.push(self.newStack,3)
-        assert self.s.checkSize(self.newStack) == 1
-    def test_needsfiles(tmp_path):
-        print(tmp_path)
-        assert 0
+        assert self.s.checkSize(self.newStack) == 2
     
