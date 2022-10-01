da254cc563de8fad15ca42cd4aeaf93fa862c06d
murtaza63 <murtaza63@gmail.com>
2022-10-01 15:39:30 +0500

    treeNode is added

--- GeneralPurposeTree/TreeNode.py
+++ GeneralPurposeTree/TreeNode.py
@@ -2,10 +2,68 @@
 
 
 class TreeNode:
-    children = [TreeNode] = []
+   
     def __init__(self, value):
         self.value = value 
+        self.children = []
+        self.parent = None
     
-    def add(self, child: TreeNode):
+    def add(self, child):
+        child.parent = self
         self.children.append(child)
+    
+    def forEachDepthFirst(self):
+        self.forEachDepthFirst()
+        if self.children:
+            for child in self.children:
+                return child
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+        return level
+    def print_tree(self):
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix + self.value)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+
+
+def build_tree():
+    root = TreeNode("Electronics")
+
+    laptop = TreeNode("Laptop")
+    laptop.add(TreeNode("Mac"))
+    laptop.add(TreeNode("Surface"))
+
+    laptop.add(TreeNode("Thinkpad"))
+
+
+    cellphone = TreeNode("Cell Phone")
+    cellphone.add(TreeNode("iPhone"))
+    cellphone.add(TreeNode("Google Pixel"))
+    cellphone.add(TreeNode("Vivo"))
+
+    tv = TreeNode("TV")
+    tv.add(TreeNode("LG"))
+    tv.add(TreeNode("Sony"))
+    tv.add(TreeNode("CHINA"))
+    root.add(laptop)
+    root.add(cellphone)
+    root.add(tv)
+    
+    return root
+
+
+
+if __name__ == "__main__":
+    root = build_tree()
+    root.print_tree()
+    # print(root.get_level())
+    
+    pass
     
---
+++ Queue/QueueChallenges.py/whosTurn.py
@@ -0,0 +1,13 @@
+from Queue import QueryArray
+import Queue
+
+class BoardGameManager:
+    def nextPlayer(self):
+        person = QueryArray 
+
+        if person:
+            return person 
+        else:
+            return None 
+        Queue.enqueue(person)
+        return person
